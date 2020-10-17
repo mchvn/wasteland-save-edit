a322601120b7eb3d8b537ba3d27408894111b7bd
Michael <mail@chovan.co>
2020-10-17 12:58:03 -0400

    moving button to own file

---
+++ wasteland-edit/src/components/Button.js
@@ -0,0 +1,14 @@
+export default Button = styled.button`
+  padding: 10px;
+  background-color: #444;
+  cursor: pointer;
+  margin: 10px 10px 10px 0;
+  border: 0px solid;
+  font-size: 12;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  &:hover {
+    color: black;
+  }
+`
